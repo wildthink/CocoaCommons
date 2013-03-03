2dcb58fb301bba71e3d47031984d41c62c88abf4
Jason Jobe <jason2010@jasonjobe.com>
2013-03-02 19:02:52 -0500

    iOS Sampler

---
+++ Sampler/iSamplerTests/iSamplerTests.h
@@ -0,0 +1,13 @@
+//
+//  iSamplerTests.h
+//  iSamplerTests
+//
+//  Created by Jason Jobe on 3/2/13.
+//  Copyright (c) 2013 Jason Jobe. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface iSamplerTests : SenTestCase
+
+@end
---
+++ Sampler/iSamplerTests/iSamplerTests.m
@@ -0,0 +1,32 @@
+//
+//  iSamplerTests.m
+//  iSamplerTests
+//
+//  Created by Jason Jobe on 3/2/13.
+//  Copyright (c) 2013 Jason Jobe. All rights reserved.
+//
+
+#import "iSamplerTests.h"
+
+@implementation iSamplerTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in iSamplerTests");
+}
+
+@end
