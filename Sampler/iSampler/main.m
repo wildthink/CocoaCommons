be633c62ea1e69933cf008e7906486af59b19c9e
Jason Jobe <jason2010@jasonjobe.com>
2013-03-02 19:02:13 -0500

    iOS Sampler starter

---
+++ Sampler/iSampler/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  iSampler
+//
+//  Created by Jason Jobe on 3/2/13.
+//  Copyright (c) 2013 Jason Jobe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
